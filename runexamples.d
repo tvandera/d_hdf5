5b687981e9b312f85c3d116de8142e6f6ecc9e75
Tom Vander Aa <vanderaa@imec.be>
2024-09-19 12:05:39 +0100

    runexamples: some cleanup

--- runexamples.d
+++ runexamples.d
@@ -5,33 +5,19 @@ import std.stdio;
 import std.string;
 import std.exception;
 import std.process;
+
 void main(string[] args)
 {
-	string[] work;
-	string[] desc;
-	string[] file;
-	writefln("*** building list of projects to run");
 	chdir("data");
-	foreach(entry;dirEntries("../build",SpanMode.breadth))
+
+	foreach (entry; dirEntries("../build", SpanMode.breadth))
 	{
-		if ((entry.isDir)||(entry.name.baseName.indexOf(".")!=-1))
+		if ((entry.isDir) || (entry.name.baseName.indexOf(".") != -1))
 			continue;
-		else
-			work~=entry.name;
-	}
-	foreach(entry;work)
-	{
-		writefln("*** about to run %s; press enter",entry.baseName);
-		auto line=readln();
-		auto ret=executeShell(entry);
-		if (ret.status==0)
-			writefln("*** results:\n%s",ret.output);
-		else
-		{
-			writefln("**************failed");
-			writefln("output: %s", ret.output);			
-		}
-		writefln("\n");
+
+		auto ret = executeShell(entry);
+		string status = (ret.status == 0) ? "SUCCESS" : "FAILED";
+		writefln("%s: results: %s", entry, status);
 	}
 	chdir("..");
 }
